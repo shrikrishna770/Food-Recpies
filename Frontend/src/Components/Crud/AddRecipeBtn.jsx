4381b3b2bc545c962c9b5ce9c70d901c55272052
sujitkumar-13 <sujitkumar60395@gmail.com>
2025-08-31 03:19:30 +0530

    AddItem

--- Frontend/src/Components/Crud/AddRecipeBtn.jsx
+++ Frontend/src/Components/Crud/AddRecipeBtn.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 const RecipeBtn = () => {
   return (
-    <div className=" flex justify-between max-w-[1500px] w-full mx-auto items-center mt-[100px] mb-[50px]">
+    <div className="flex justify-between max-w-[1400px] w-full mx-auto items-center mt-[100px] mb-[30px] px-4">
       <h1 className="font-[500] text-[22px]">Dashboard</h1>
       <div>
-        <button className="border px-[20px] rounded-[12px] flex items-center gap-[10px] bg-green-600 text-white hover:bg-green-700"><span className="text-[28px] ">+</span> Add Recipe</button>
+        <Link to="/add-item">
+          <button className="border px-[20px] rounded-[12px] flex items-center gap-[10px] bg-green-600 text-white hover:bg-green-700">
+            <span className="text-[28px]">+</span> Add Recipe
+          </button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecipeBtn
+export default RecipeBtn;
