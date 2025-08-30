cc81115bd459b5fca0c20af50abcfc98088ba4e6
sujitkumar-13 <sujitkumar60395@gmail.com>
2025-08-30 13:36:37 +0530

    NavBar Create

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
+import frame from "../../assets/Frame.png";
+
 const Navbar = () => {
     return (
-        <div>
-            <h1 className="bg-red-500">Hello World</h1>
+        <div className="flex  shadow-md rounded-lg fixed top-0 left-0 right-0 bg-white z-10">
+            <nav className="flex max-w-[1500px] w-[100%] h-[70px]  m-[auto] items-center justify-between ">
+                <h1 className="text-[#16A34A] font-bold text-[24px] p-[10px]  h-[28px] flex items-center"><a href="/">RecipeSync</a></h1>
+                <ul className="flex h-[36px]  gap-[50px] mr-[20px] items-center">
+                    <li><a href="/">Dashboard</a></li>
+                    <li><a href="/">My Recipe </a></li>
+                    <li><a href="/">Public Feed</a></li>
+                    <li className=" flex items-center gap-2 p-2">
+                        <a href="/" className="flex items-center gap-2">
+                            <img src={frame} alt="Logout icon" className="w-5 h-5" />
+                            <span>Logout</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </nav>
         </div>
+
     )
 }
-
 export default Navbar
